d8fd2f1f839541b8eaeffa560231aa9750aa924e
wentao <zcabwei@ucl.ac.uk>
2017-02-22 14:03:58 +0000

    update

--- RMPSD/src/rmpsd/RMPSD.java
+++ RMPSD/src/rmpsd/RMPSD.java
@@ -11,18 +11,18 @@ package rmpsd;
  * @author g2arc
  */
 public class RMPSD {
-
-
-
+    
 
     private static final int INF = Integer.MAX_VALUE;
     public static void main(String[] args) {
-      double[] coordinates={-1.5,1.5,-1,0,5,0,4.5,3.5,4.6,-3,0,1,2,3,4,1,4,10,0,10,4,0,2,2,0,0,0,-10,4,-10};
-      int[] index={10,20,30};
+//      double[] coordinates={-1.5,1.5,-1,0,5,0,4.5,3.5,4.6,-3,0,1,2,3,4,1,4,10,0,10,4,0,2,2,0,0,0,-10,4,-10};
+//      int[] index={10,20,30};
+      double[] coordinates={-1.5,1.5,-1,0,5,0,4.5,3.5,4.6,-3};
+      int[] index={-1};
       rmp newrmp=new rmp(coordinates,index);
       //newrmp.findPath(0, 2);
-      System.out.println(newrmp.getShortestDistance(0,2));
-      int route[]=newrmp.getShortestRoute(0, 2);
+      System.out.println(newrmp.getShortestDistance(0,4));
+      int route[]=newrmp.getShortestRoute(0, 4);
       for(int i=0;i<route.length;i++)
       {
           if(route[i]!=-1)
@@ -30,7 +30,7 @@ public class RMPSD {
           else{break;}
       }
       System.out.println();
-      double coordinate[]=newrmp.getCoordinates(11);
+      double coordinate[]=newrmp.getCoordinates(1);
       for(int i=0;i<coordinate.length;i++)
       {
           System.out.print(coordinate[i]+" ");
